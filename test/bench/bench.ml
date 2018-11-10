6da80780a6ef5924c94eeb943ad707c061355fff
Marco Aurélio da Silva <marcoonroad@gmail.com>
2018-11-10 14:52:30 -0200

    test: benchmarks for signing / verification
    Signed-off-by: Marco Aurélio da Silva <marcoonroad@gmail.com>

--- test/bench/bench.ml
+++ test/bench/bench.ml
@@ -1,11 +1,3 @@
-(* TODO
-
-   Run a benchmark test where our implementation must be fast enough in given
-   metrics (for instance, signing must be faster than X.YZ seconds). A good library
-   to create benchmarks is the Jane Street's core_bench.
-
-*)
-
 open Core_bench.Bench
 module Command = Core.Command
 
--- test/bench/rsa_pss.ml
+++ test/bench/rsa_pss.ml
@@ -10,7 +10,30 @@ let priv = Rsa.generate 1024
 let derive ( ) =
   ignore (Rsa.pub_of_priv priv)
 
+let signing ( ) =
+  "RSA PSS signed message."
+  |> Cstruct.of_string
+  |> Alg.sign ~key:priv
+  |> Cstruct.to_string
+  |> ignore
+
+let signature =
+  "RSA PSS signing message"
+  |> Cstruct.of_string
+  |> Alg.sign ~key:priv
+  |> Cstruct.to_string
+
+let pub = Rsa.pub_of_priv priv
+
+let verification ( ) =
+  "RSA PSS signing message"
+  |> Cstruct.of_string
+  |> Alg.verify ~key:pub ~signature:(Cstruct.of_string signature)
+  |> ignore
+
 let suite = [
-  Test.create ~name:"rsa pss 1024 bits private key generation" generate;
-  Test.create ~name:"rsa pss 1024 bits public key derivation"  derive
+  Test.create ~name:"rsa pss/sha256 1024 bits private key generation"  generate;
+  Test.create ~name:"rsa pss/sha256 1024 bits public key derivation"   derive;
+  Test.create ~name:"rsa pss/sha256 1024 bits private key signing"     signing;
+  Test.create ~name:"rsa pss/sha256 1024 bits public key verification" verification
 ]
