fd64db9e5ae3eff8f0e36acf82b4aa7eed2a3323
Marco Aurélio da Silva <marcoonroad@gmail.com>
2018-11-13 02:05:19 -0200

    refactor: irmin git set-blacklist backend instead sole filesystem
    Signed-off-by: Marco Aurélio da Silva <marcoonroad@gmail.com>

--- lib/blacklist.ml
+++ lib/blacklist.ml
@@ -1,20 +1,47 @@
-module Sys = Core.Sys
 module Unix = Core.Unix
-module Out_channel = Core.Out_channel
+module String = Core.String
+module Sys = Core.Sys
+
+open Lwt.Infix
+
+module Store = Irmin_unix.Git.FS.KV(Irmin.Contents.String)
+
+let split address =
+  let length = String.length address in
+  let directory = String.sub ~pos:2 ~len:2 address in
+  let filename = String.sub ~pos:4 ~len:(length - 4) address in
+  (directory, filename)
 
 let _HOME = Sys.getenv_exn "HOME"
-let _BLACKLIST_ROOTDIR = _HOME ^ "/.hieroglyphs-blacklist"
+let _CONFIG_DIR = _HOME ^ "/.hieroglyphs"
+let _BLACKLIST_ROOTDIR = _CONFIG_DIR ^ "/blacklist"
+
+let config = Irmin_git.config ~bare:true _BLACKLIST_ROOTDIR
+let info fmt = Irmin_unix.info fmt
 
 let add address =
-  let filename = _BLACKLIST_ROOTDIR ^ "/" ^ address in
-  let filehandler = Out_channel.create ~append:false ~binary:false ~fail_if_exists:true filename in
-  Out_channel.output_string filehandler "";
-  Out_channel.close filehandler
+  let (directory, filename) = split address in
+  let path = [directory; filename] in
+  let msg = info "Revoking Private Key." in
+  let transaction ( ) =
+    Store.Repo.v config >>= function repo ->
+    Store.master repo >>= function branch ->
+    Store.set branch ~info:msg path ""
+  in
+  Lwt_main.run (transaction ( ))
 
 let exists address =
-  let open Sys in
-  let filename = _BLACKLIST_ROOTDIR ^ "/" ^ address in
-  file_exists_exn ~follow_symlinks:true filename
+  let (directory, filename) = split address in
+  let path = [directory; filename] in
+  let transaction ( ) =
+    Store.Repo.v config >>= function repo ->
+    Store.master repo >>= function branch ->
+    Store.get branch path
+  in
+  try ignore(Lwt_main.run (transaction ( ))); true
+  with Invalid_argument _ -> false
 
 let ( ) =
-  Unix.mkdir_p _BLACKLIST_ROOTDIR
+  Unix.mkdir_p _CONFIG_DIR;
+  Unix.mkdir_p _BLACKLIST_ROOTDIR;
+  Irmin_unix.set_listen_dir_hook ()
