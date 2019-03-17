6efc8b02356d1a755f2d53d33a9f6e8a8db2273f
Marco Aurélio da Silva <marcoonroad@gmail.com>
2019-03-16 22:40:29 -0300

    refactor: :recycle: :closed_lock_with_key: :eyes: dropping AES ECB by AES CBC for private key encryption
    Signed-off-by: Marco Aurélio da Silva <marcoonroad@gmail.com>

--- lib/encryption.ml
+++ lib/encryption.ml
@@ -1,17 +1,28 @@
-open Nocrypto.Cipher_block.AES.ECB
+open Nocrypto.Cipher_block.AES.CBC
 module Option = Core.Option
 module Base64 = Nocrypto.Base64
 
+let digest cstruct =
+  let digest = Nocrypto.Hash.SHA256.digest cstruct in
+  let left, right = Cstruct.split digest 16 in
+  (* split 32 bytes into a pair of 16 *)
+  Nocrypto.Uncommon.Cs.xor left right
+
+
 let encrypt msg ~pass =
-  let key = of_secret (Hash.mine pass ~difficulty:5) in
-  let result = encrypt ~key (Utils.pad ~basis:16 msg) in
+  let proof = Hash.mine pass ~difficulty:5 in
+  let key = of_secret proof in
+  let iv = digest proof in
+  let result = encrypt ~iv ~key (Utils.pad ~basis:16 msg) in
   result |> Base64.encode |> Cstruct.to_string
 
 
 let decrypt cipher ~pass =
-  let key = of_secret (Hash.mine pass ~difficulty:5) in
+  let proof = Hash.mine pass ~difficulty:5 in
+  let key = of_secret proof in
+  let iv = digest proof in
   let result = cipher |> Cstruct.of_string |> Base64.decode in
   let open Option in
   result
   >>= fun msg ->
-  msg |> decrypt ~key |> Cstruct.to_string |> Utils.unpad |> some
+  msg |> decrypt ~iv ~key |> Cstruct.to_string |> Utils.unpad |> some
