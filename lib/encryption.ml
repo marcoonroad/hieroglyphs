c9657a1e15288e08cabb875f229bcd7d31472468
Marco Aurélio da Silva <marcoonroad@gmail.com>
2019-07-13 17:42:42 -0300

    refactor: :recycle: using Blake2B instead of SHA256 to derive IV from proof-of-work pass on Encryption module
    Signed-off-by: Marco Aurélio da Silva <marcoonroad@gmail.com>

--- lib/encryption.ml
+++ lib/encryption.ml
@@ -2,17 +2,26 @@ open Nocrypto.Cipher_block.AES.CBC
 module Option = Core.Option
 module Base64 = Nocrypto.Base64
 
-let digest cstruct =
-  let digest = Nocrypto.Hash.SHA256.digest cstruct in
-  let left, right = Cstruct.split digest 16 in
-  (* split 32 bytes into a pair of 16 *)
-  Nocrypto.Uncommon.Cs.xor left right
+let __digest cstruct =
+  Cstruct.of_hex @@ Hash.digest @@ Cstruct.to_string cstruct
+
+
+let __compute_iv cstruct =
+  let digest = __digest cstruct in
+  (* split 64 bytes into a pair of 32 *)
+  let blob1, blob2 = Cstruct.split digest 32 in
+  (* then split 32 bytes into a pair of 16 *)
+  let blob3, blob4 = Cstruct.split blob1 16 in
+  let blob5, blob6 = Cstruct.split blob2 16 in
+  let blob7 = Nocrypto.Uncommon.Cs.xor blob3 blob4 in
+  let blob8 = Nocrypto.Uncommon.Cs.xor blob5 blob6 in
+  Nocrypto.Uncommon.Cs.xor blob7 blob8
 
 
 let encrypt msg ~pass =
   let proof = Hash.mine pass ~difficulty:5 in
   let key = of_secret proof in
-  let iv = digest proof in
+  let iv = __compute_iv proof in
   let result = encrypt ~iv ~key (Utils.pad ~basis:16 msg) in
   result |> Base64.encode |> Cstruct.to_string
 
@@ -20,7 +29,7 @@ let encrypt msg ~pass =
 let decrypt cipher ~pass =
   let proof = Hash.mine pass ~difficulty:5 in
   let key = of_secret proof in
-  let iv = digest proof in
+  let iv = __compute_iv proof in
   let result = cipher |> Cstruct.of_string |> Base64.decode in
   let open Option in
   result
